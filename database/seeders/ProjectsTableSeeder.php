dd74409b7371def31e46d09df3edabda7fa141a8
alex2000d <demarcoalessandro777@gmail.com>
2024-10-03 21:57:43 +0200

    [UPD] created the database

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10 ; $i++) { 
+            $project =  new project();
+            $project->name_project = $faker->sentence(3);  
+            $project->description = $faker->paragraph();  
+            $project->name = 'Alessandro';  
+            $project->surname = 'De_Marco';  
+            $project->github = $faker->url();  
+            
+            $project->save();
+         }
+    }
+}
