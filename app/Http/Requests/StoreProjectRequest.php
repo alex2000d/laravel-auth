add1dc0393d9f75c35ad54c40f4ed17b211c3be1
alex2000d <demarcoalessandro777@gmail.com>
2024-10-04 21:31:57 +0200

    [UPD] errors message done and created the edit file

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -31,5 +31,22 @@ class StoreProjectRequest extends FormRequest
             'github' => 'required|max:255',
 
         ];
+       
+    }
+
+    public function messages(){
+            
+        return [
+             'name_project.required' => 'il nome del progetto è obbligatorio',
+             'name_project.max' => 'il nome del progetto deve essere lungo al massimo 255 caratteri',
+             'description.required' => 'la descrizione è obbligatoria',
+             'description.max' => 'la descrizione deve essere lunga al massimo 255 caratteri',
+             'name.required' => 'il nome è obbligatorio',
+             'name.max' => 'il nome deve essere lunga al massimo 255 caratteri',
+             'surname.required' => 'il cognome è obbligatorio',
+             'surname.max' => 'il cognome deve essere lungo al massimo 255 caratteri',
+             'github.required' => 'il link github è obbligatorio',
+             'surname.max' => 'il link github deve essere lungo al massimo 255 caratteri',
+         ];
     }
 }
